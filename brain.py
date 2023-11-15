9c03bb773ffd00be111d5c8532350568b88aa0eb
juk-r <->
2023-11-15 19:24:59 +0300

    refactor abstract brain

--- brain.py
+++ brain.py
@@ -1,23 +1,50 @@
 import abc
 import typing
 
+from robot import Robot
+
 if typing.TYPE_CHECKING:
     import simpy
-    from robot import Robot
     from structures import Map
 
 class Brain(abc.ABC):
     @property
     def map(self):
         return self._map
+    @property
+    def count(self):
+        return self._count
 
     def __init__(self, env: "simpy.Environment", map_: "Map"):
         self._env = env
         self._map = map_
-        self._idle_robots: list["Robot"] = []
-        self._charging_robots: list["Robot"] = []
+        self._idle_robots: list[Robot] = []
+        self._charging_robots: list[Robot] = []
+        self._input_destinations: dict[Robot, int] = {}
+        self._count = 0
 
+
+    def get_next_action(self, robot: Robot) -> Robot.Action:
+        if robot.mail is not None:
+            if robot.position == self._map.outputs[robot.mail.destination]:
+                self._mail_put(robot)
+                self._count += 0
+                return Robot.Action.put
+            return self._go_with_mail(robot, robot.mail.destination)
+        else:
+            if robot.position == self._map.inputs[self._input_destinations[robot]]:
+                self._mail_taken(robot)
+                return Robot.Action.take
+            return self._go_without_mail(robot, self._input_destinations[robot])
 
     @abc.abstractmethod
-    def get_next_action(self, robot: "Robot") -> "Robot.Action":
+    def _go_with_mail(self, robot: Robot, destination: int) -> Robot.Action: ...
+
+    @abc.abstractmethod
+    def _go_without_mail(self, robot: Robot, destination: int) -> Robot.Action: ...
+
+    def _mail_taken(self, robot: Robot):
+        pass
+
+    def _mail_put(self, robot: Robot):
         pass
--- brains/direction_brain.py
+++ brains/direction_brain.py
@@ -3,38 +3,38 @@ import simpy
 import collections
 
 from brain import Brain
-from maps import DirectionMap
 from robot import Robot
+
+if typing.TYPE_CHECKING:
+    from maps import DirectionMap
+    from structures import Direction
 
 
 class DirectionBrain(Brain):
-    @property
-    def count(self):
-        return self._count
-    def __init__(self, env: simpy.Environment, map_: DirectionMap):
+    def __init__(self, env: simpy.Environment, map_: "DirectionMap"):
         super().__init__(env, map_)
         self._map: DirectionMap
         self._destinations = collections.defaultdict[Robot, int](
             lambda: self._next_input(None))
-        self._count = 0
         self._last = 0
 
     def _next_input(self, robot: Robot | None):
         self._last += 1
         return self._map.input_ids[self._last % len(self._map.input_ids)]
-    
-    @typing.override
-    def get_next_action(self, robot: Robot) -> Robot.Action:
-        if robot.mail is not None:
-            if robot.position == self._map.outputs[robot.mail.destination]:
-                self._destinations[robot] = self._next_input(robot)
-                self._count += 1
-                return Robot.Action.put
-            direction = self._map[robot.position].to_outputs[robot.mail.destination]
-        else:
-            if robot.position == self._map.inputs[self._destinations[robot]]:
-                return Robot.Action.take
-            direction = self._map[robot.position].to_inputs[self._destinations[robot]]
+
+    def _turn_move(self, robot: "Robot", direction: "Direction"):
         if robot.direction == direction:
             return robot.Action.move
         return Robot.Action.turn_to(direction)
+
+    @typing.override
+    def _go_with_mail(self, robot: "Robot", destination: int):
+        return self._turn_move(robot, self._map[robot.position].to_outputs[destination])
+
+    @typing.override
+    def _go_without_mail(self, robot: "Robot", destination: int) -> "Robot.Action":
+        return self._turn_move(robot, self._map[robot.position].to_inputs[destination])
+
+    @typing.override
+    def _mail_put(self, robot: Robot):
+        self._destinations[robot] = self._next_input(robot)
